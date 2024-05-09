8b1f60e5c751598dc90f1740645842a54586faf4
unique_hunter <77807213+oguz162@users.noreply.github.com>
2024-05-09 18:32:25 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,24 +3,24 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "95516491871"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘Zlose✘𓆪_✰",
+global.ytname = "..."
+global.socialm = "..."
+global.location = "Turkey"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Alpha-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙𓆩‎✘Zlose✘𓆪_✰",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "💙𓆩‎✘Zlose✘𓆪_✰",
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Zlose."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+global.link = '...'
 
 //custom prefix
 global.prefa = process.env.PREFIX || ".",
@@ -60,7 +60,7 @@ ANTI_BOT:"true",
 }
 
 
-global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
+global.thumb = fs.readFileSync('./Gallery/ai-generated-8015426_1280.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
